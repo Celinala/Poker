2c333c7e2d8b9842563bedbfc10d20ad248c83a2
festevez10 <estevezfernando1995@gmail.com>
2017-11-13 16:36:22 -0800

    Skeliton for the poker program

--- Poker/src/Poker.cpp
+++ Poker/src/Poker.cpp
@@ -9,6 +9,50 @@
 #include <iostream>
 using namespace std;
 
+class Cards{
+	enum Suit{ HEART, CLUBS, SPADES, DIAMONDS};
+	enum value{TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK,QUEEN, KING, ACE};
+
+	Cards(string value, string cardSuit){
+
+	}
+};
+
+class Players{
+	Player(string name){
+
+	}
+
+	string displayName(){
+		//displays the player name
+		return 0;
+	}
+
+	void hand(){
+		//give appropriate number of cards to this player
+	}
+
+	void showHand(){
+		//shows all five cards
+	}
+
+};
+
+class Dealer{
+	Dealer(){
+		//assign 52 cards to the deck
+	}
+
+	void shuffleDeck(){
+		//shuffles deck of cards using rand or srand
+	}
+	void dealCards(){
+		//hands out cards to player 1 at a time
+	}
+	string winner(){
+		//logic to decide who is the winner
+	}
+};
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout<<"hi celina"<<endl;
